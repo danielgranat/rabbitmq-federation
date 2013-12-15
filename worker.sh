332e0ca7aaa4ddeff648a8a13a4d7d5c776fe31f
danielgranat <daniel.granat@gmail.com>
2013-12-15 16:08:55 +0200

    install dependencies to run bandle

--- worker.sh
+++ worker.sh
@@ -1,7 +1,10 @@
-
 apt-get update
 
 apt-get install -q -y tmux screen htop vim curl wget build-essentials git-core
 apt-get install -q -y ruby1.9.3
+apt-get install -q -y make
+apt-get install -q -y g++
 
 gem install bundler
+
+bandle
