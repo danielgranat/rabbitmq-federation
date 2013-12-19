2b577f4ddfb15ac5bd98a469281eb8d61d4a74cd
danielgranat <daniel.granat@gmail.com>
2013-12-19 15:43:29 +0200

    install git

--- worker.sh
+++ worker.sh
@@ -4,6 +4,7 @@ apt-get install -q -y tmux screen htop vim curl wget build-essentials git-core
 apt-get install -q -y ruby1.9.3
 apt-get install -q -y make
 apt-get install -q -y g++
+apt-get install -q -y git
 
 gem install bundler
 
